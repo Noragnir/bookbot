39a9396f1b09f8da46dbc2de2c5f7657951691a8
Noragnir <barith9@gmail.com>
2025-01-04 21:35:05 -0600

    count_characters codded

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ def count_words(file_contents):
         count = len(word)
         return(count)
 
+def count_characters(file_contents):
+        lowered_file = file_contents.lower()
+        char_count ={}
+        for char in lowered_file:
+                if char in char_count:
+                        char_count[char] += 1
+                else:
+                        char_count[char] = 1
+        return char_count
+
 def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
         word_count = count_words(file_contents)
+        cara_count = count_characters(file_contents)
         print(word_count)
+        print(cara_count)
 main()
+
+
